00de368164752f50250a03f463b786461613600b
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-21 10:45:46 +0530

    lambda function

---
+++ func4_ADID.py
@@ -0,0 +1,11 @@
+
+avg1 = lambda p, q: p+q / 2
+
+res1_UserID = avg1(30, 20)
+print(res1_UserID)  #40.0
+
+res2_UserID = avg1(1.7, 1.3)
+print(res2_UserID)  #2.35
+
+res3_UserID = avg1(30.0, 2)
+print(res3_UserID)  #31.0
